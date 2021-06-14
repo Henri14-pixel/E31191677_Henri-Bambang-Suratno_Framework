37f679b929604aadf92fa842907f3ed552cafdf9
Henri Bambang <henribambang5@gmail.com>
2021-06-14 11:20:21 +0700

    CRUD (Update Delete)

---
+++ minggu12/ci/application/views/admin/_template/footer.php
@@ -0,0 +1,10 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            Copyright &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script> All rights reserved by Henri Bambang S-E31191677
+        </div>
+    </div>
+</footer>
